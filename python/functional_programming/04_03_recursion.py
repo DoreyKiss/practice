0ad78b8ba3689d45fae9fe75d54b450bb6b25a89
DoreyKiss <dorey.kiss@gmail.com>
2025-05-31 22:09:43 +0200

    better typing for functionals part 2

--- python/functional_programming/04_03_recursion.py
+++ python/functional_programming/04_03_recursion.py
@@ -1,17 +1,20 @@
-def count_down(x):
+def count_down(x: int) -> None:
     if x < 0:
-        print('Done!')
+        print("Done!")
         return
     print(x)
     count_down(x - 1)
 
+
 count_down(10)
 
-def count_up(x, maximum):
+
+def count_up(x: int, maximum: int) -> None:
     if x > maximum:
-        print('Done!')
+        print("Done!")
         return
     print(x)
     count_up(x + 1, maximum)
+
 
 count_up(0, 10)
