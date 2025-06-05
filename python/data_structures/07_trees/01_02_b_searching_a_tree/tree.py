94d3e6ae118f2734c47814c9c95c0c9ccda8f41a
DoreyKiss <dorey.kiss@gmail.com>
2025-06-05 14:22:49 +0200

    data structures and trees in python

--- python/data_structures/07_trees/01_02_b_searching_a_tree/tree.py
+++ python/data_structures/07_trees/01_02_b_searching_a_tree/tree.py
@@ -1,11 +1,12 @@
 class Node:
     def __init__(self, data):
         self.data = data
-        self.left = None
-        self.right = None
+        self.left: Node | None = None
+        self.right: Node | None = None
+
 
 class Tree:
-    def __init__(self, root, name=''):
+    def __init__(self, root, name=""):
         self.root = root
         self.name = name
 
@@ -21,11 +22,4 @@ node.left.right = Node(6)
 node.right.left = Node(13)
 node.right.right = Node(10000)
 
-
-
-myTree = Tree(node, 'Ryan\'s Tree')
-
-print(myTree.name)
-print(myTree.root.left.data)
-
-print(myTree.root.right.right.data)     
+myTree = Tree(node, "Ryan's Tree")
